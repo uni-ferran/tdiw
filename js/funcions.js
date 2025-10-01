2e66c89ffc5d8eaf33c88b3a326772f8dda3f44b
Ferran Fernandez <ferran.fernandezS@autonoma.cat>
2025-10-01 11:22:54 +0200

    done

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,13 @@
-function confirmaRegistre(){
-    //completa
-}
+// js/funcions.js
+// 1–6. confirmaRegistre: alert, console log, reemplazar formulario y return false
+
+function confirmaRegistre() {
+    // 2. Ventana alert
+    alert("Registrant usuari")
+    // 3. Mensaje en la consola
+    console.log("Registrant usuari");
+    // 4. Reemplazar el contenido del div formDiv por un párrafo con clase important
+    let formDiv = document.getElementById("formDiv")
+    formDiv.innerHTML = "<p class='important'>Usuari registrat amb èxit</p>";
+    return false // 5. Evitar el envío del formulario
+    }
